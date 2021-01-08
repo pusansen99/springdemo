5bb9a91d0d9dfc1e359cb3907c81bdee0d3c4939
extern.fordonski_bartlomiej <extern.fordonski_bartlomiej@allainz.de>
2021-01-08 19:39:35 +0530

    application files

---
+++ src/main/java/com/deloitte/spring/hello/HelloController.java
@@ -0,0 +1,11 @@
+package com.deloitte.spring.hello;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String HelloWorld() {
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/deloitte/spring/topic/TopicService.java
@@ -0,0 +1,26 @@
+package com.deloitte.spring.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("1", "Spring",  "Spring Description"),
+            new Topic("2", "Java", "Java Description"),
+            new Topic("3", "Python", "Python Description")
+    ));
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+}
